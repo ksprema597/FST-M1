3568f1c0d8dc835f3fa3727d1c861a6b2e502a90
ksprema597 <91205798+ksprema597@users.noreply.github.com>
2025-08-09 01:00:02 +0530

    Add files via upload

---
+++ Python/Activities/activity2.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+mod = num % 2
+if mod > 0:
+    print("{} is even".format(num))
+else:
+     print("You picked an even number.")
---
+++ Python/Activities/activity7.py
@@ -0,0 +1,7 @@
+numList = [10,20,30,40,50,60]
+
+#cal
+sum = 0
+for item in numList:
+    sum =   sum+item
+print("The sum of the numbers in the list is:",sum)
